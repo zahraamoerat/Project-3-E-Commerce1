7eb69fead8d30261d16786d3902acd4d278290e3
Zandakumsha <zandaj.kumsha@gmail.com>
2026-09-19 17:03:15 +0200

    mount categories API

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import { testDatabaseConnection } from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import supplierRoutes from "./routes/supplierRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import path from "path";
 
 dotenv.config();
@@ -78,6 +79,7 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/supplier", supplierRoutes);
 app.use("/api/uploads", uploadRoutes);
+app.use("/api/categories", categoryRoutes);
 
 /* =========================
    ERROR HANDLER
