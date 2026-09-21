f53f3951ff634b04afc58dbcb50a2ba4792cde65
Zandakumsha <zandaj.kumsha@gmail.com>
2026-09-21 21:40:52 +0200

    Made fixes

--- backend/src/controllers/b_deliveryController.js
+++ backend/src/controllers/b_deliveryController.js
@@ -6,7 +6,7 @@ import {
 // Return all deliveries to the frontend.
 export async function fetchDeliveries(req, res) {
   try {
-    const buyerId = req.query.buyerId ? Number(req.query.buyerId) : null;\n    const deliveries = await getAllDeliveries(buyerId);
+    const buyerId = req.query.buyerId ? Number(req.query.buyerId) : null;    const deliveries = await getAllDeliveries(buyerId);
 
     res.json(deliveries);
   } catch (error) {
--- backend/src/controllers/b_orderController.js
+++ backend/src/controllers/b_orderController.js
@@ -6,7 +6,8 @@ import {
 // Return all orders to the frontend.
 export async function fetchOrders(req, res) {
   try {
-    const buyerId = req.query.buyerId ? Number(req.query.buyerId) : null;\n    const orders = await getAllOrders(buyerId);
+    const buyerId = req.query.buyerId ? Number(req.query.buyerId) : null;
+    const orders = await getAllOrders(buyerId);
 
     res.json(orders);
   } catch (error) {
@@ -23,7 +24,7 @@ export async function fetchOrderById(req, res) {
   try {
     const { orderId } = req.params;
 
-    const buyerId = req.query.buyerId ? Number(req.query.buyerId) : null;\n    const order = await getOrderById(orderId, buyerId);
+    const buyerId = req.query.buyerId ? Number(req.query.buyerId) : null;  const order = await getOrderById(orderId, buyerId);
 
     if (!order) {
       return res.status(404).json({
