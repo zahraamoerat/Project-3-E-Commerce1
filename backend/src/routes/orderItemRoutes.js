ab57452a181777930690dc923748b363b58837f3
Zandakumsha <zandaj.kumsha@gmail.com>
2026-09-21 20:01:42 +0200

    Remove unprefixed orderItemRoutes.js

--- backend/src/routes/orderItemRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { fetchOrderItems } from '../controllers/orderItemController.js';
-
-const router = express.Router();
-
-// GET /api/orders/:orderId/items
-router.get('/:orderId/items', fetchOrderItems);
-
-export default router;
