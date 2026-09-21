9dbabbeb39c751dce058b771b0704d777be8ab80
Zandakumsha <zandaj.kumsha@gmail.com>
2026-09-21 20:00:42 +0200

    Update router imports for b_ prefixed Vue pages

--- frontend/weconnect/src/router/index.js
+++ frontend/weconnect/src/router/index.js
@@ -12,61 +12,61 @@ const router = createRouter({
     {
       path: '/tracking/:deliveryId',
       name: 'tracking',
-      component: () => import('../views/TrackingView.vue'),
+      component: () => import('../views/b_TrackingView.vue'),
     },
 
     // Shared dashboard page with the GPS preview.
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: () => import('../views/Dashboard.vue'),
+      component: () => import('../views/b_Dashboard.vue'),
     },
 
     // Add the Small Business Orders page
     {
       path: '/small-business/orders',
       name: 'small-business-orders',
-      component: () => import('../views/SmallBusinessOrdersView.vue'),
+      component: () => import('../views/b_SmallBusinessOrdersView.vue'),
     },
 
     {
       path: '/small-business/products',
       name: 'small-business-products',
-      component: () => import('../views/SmallBusinessProductsView.vue'),
+      component: () => import('../views/b_SmallBusinessProductsView.vue'),
     },
 
     {
       path: '/small-business/products/:productId',
       name: 'small-business-product',
-      component: () => import('../views/SmallBusinessProductsView.vue'),
+      component: () => import('../views/b_SmallBusinessProductsView.vue'),
     },
 
     // Add the Supplier Orders page
     {
       path: '/supplier/orders',
       name: 'supplier-orders',
-      component: () => import('../views/SupplierOrdersView.vue'),
+      component: () => import('../views/b_SupplierOrdersView.vue'),
     },
 
     // Add the Small Business Deliveries page
     {
       path: '/small-business/deliveries',
       name: 'small-business-deliveries',
-      component: () => import('../views/SmallBusinessDeliveriesView.vue'),
+      component: () => import('../views/b_SmallBusinessDeliveriesView.vue'),
     },
 
     // Add the Supplier Deliveries page
     {
       path: '/supplier/deliveries',
       name: 'supplier-deliveries',
-      component: () => import('../views/SupplierDeliveriesView.vue'),
+      component: () => import('../views/b_SupplierDeliveriesView.vue'),
     },
 
     // Add the payment page
     {
       path: '/payment/:orderId',
       name: 'payment',
-      component: () => import('../views/PaymentsView.vue'),
+      component: () => import('../views/b_PaymentsView.vue'),
     },
   ],
 })
