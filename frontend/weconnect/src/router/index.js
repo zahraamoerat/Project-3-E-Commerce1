c84424454fd1735b9f06553bec1d4f9ee69b0795
Zandakumsha <zandaj.kumsha@gmail.com>
2026-09-21 15:30:19 +0200

    Remove stray cd statement from router

--- frontend/weconnect/src/router/index.js
+++ frontend/weconnect/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-cd;
 import MainLayout from "../layouts/MainLayout.vue";
 import Products from "../views/S_Products.vue";
 import Reviews from "../views/Reviews.vue";
