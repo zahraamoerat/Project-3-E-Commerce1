a4ce94a985e4221e677fbdae82ce136c2dee3040
Zandakumsha <zandaj.kumsha@gmail.com>
2026-09-21 20:30:55 +0200

    Load shared application styles

--- frontend/weconnect/src/main.js
+++ frontend/weconnect/src/main.js
@@ -1,12 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './stores'
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./stores";
+import "./assets/main.css";
 
-const app = createApp(App)
-
-app.use(router)
-app.use(store)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount("#app");
